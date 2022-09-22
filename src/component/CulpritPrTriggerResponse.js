a14849816b6a04d8caced99ca20d59014999dedf
Pankaj Boola <pankajboola@microsoft.com>
2022-09-22 23:31:15 +0530

    initial commit

---
+++ src/component/CulpritPrTriggerResponse.js
@@ -0,0 +1,89 @@
+import React, { Profiler } from 'react';
+import '../css/form.css';
+import '../css/table.css';
+import '../css/cards.css';
+
+export default class CulpritPrTriggerResponse extends React.Component {
+    constructor(props){
+        super();
+        this.state = {
+            triggerApiResponseParams: props.triggerApiResponseParams
+        };
+
+        this.handleStatusCheckUriClick = this.handleStatusCheckUriClick.bind(this);
+        this.contactCandidate = this.contactCandidate.bind(this);
+      };
+
+      handleStatusCheckUriClick(event) {
+        event.preventDefault();
+        this.setState({checkStatus: true});
+        console.log('CulpritPrResponse props: ' + JSON.stringify(this.props));
+        this.props.parentCallback({
+            showTriggerApiResponseTable: true, 
+            checkStatus: true,
+            triggerApiResponseParams: this.state.triggerApiResponseParams
+        });
+      }
+
+      async contactCandidate(event, candidateEmailId){
+        event.preventDefault();
+        this.setState({checkStatus: true});
+        console.log('Contacted candidate ');
+        console.log('this.state.triggerApiResponseParams : ', this.state.triggerApiResponseParams);
+
+        var body = {
+            "CandidateEmailID": candidateEmailId,
+            "RecruiterEmailID": "pankajboola@microsoft.com",
+            "IsCandidate": "false",
+            "IsCandidateInterested": "false",
+            "JDLink": "https://careers.microsoft.com/i/us/en/job/1467744/Cloud-Solution-Architecture"
+          };
+  
+          const requestOptions = {
+            method: 'POST',
+            mode: 'cors', 
+            headers: {
+              'Content-Type': 'application/json',
+              'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(body)
+          };
+        const response = await fetch(
+            'https://prod-26.westcentralus.logic.azure.com:443/workflows/717edc33336c4213ad455d23ee8a8311/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=spTfT0NLIO_mHlgAsKnaCkeTNTx_ieClpBeOYZloGTc', 
+            requestOptions);
+        // const data = await response.json();
+        // console.log('fetched data: response: ' + JSON.stringify(data));
+      }
+
+      render() {
+        return (
+            <div className="container">
+                {console.log('RESPONSE PANKAJ' + JSON.stringify(this.props.triggerApiResponseParams))}
+                <div>
+                
+                {
+                this.props.triggerApiResponseParams.data.map(person => 
+                (        
+                        <div className="card" key={person.id}>
+                            <div className="box">
+                              <div className="content">
+                                  <h2>{person.yoe}</h2>
+                                  <h6> YoE </h6>
+                                  <h3>{(person.is_identity_visible === "true") ? person.first_name : "Hidden"}</h3>
+
+                                  <p>{(person.is_identity_visible === "true") ? person.email_id : "Email Hidden"}</p>
+                                  <br/>
+                                  <p><span style={{"fontWeight": "bold"}}>Languages: </span>{person.languages_known}</p>
+                                  <br/>
+                                  <p><span style={{"fontWeight": "bold"}}>Interest Area: </span>{person.interest_area}</p>
+                                  <a onClick={event => this.contactCandidate(event, person.email_id)}> Contact Dev</a>
+                              </div>
+                            </div>
+                        </div>
+                ))
+                }
+                </div>
+            </div>
+        );
+      }
+}
---
+++ src/component/TriggerCulpritPR.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import '../css/form.css';
+import CulpritPrTriggerResponse from './CulpritPrTriggerResponse';
+
+import response from "../resources/response.json";
+
+class TriggerCulpritPR extends React.Component {
+    constructor(props){
+        super();
+        this.state = {
+          triggerApiRequestParams: {
+
+            recruiterEmailId: '',
+            query: '',
+            jdlink: '',
+            queryURI: 'https://prod-29.centralus.logic.azure.com/workflows/0093d7b5bf104cf3bbcec6b20b230637/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=A1RV1mWrzJkrulj9vHdoQ_wn0gC9lvg6AFqeS0QFAaE&searchquery='
+
+          },
+          triggerApiResponseParams: {
+            id: '',
+            statusQueryGetUri: 'https://hireme-engine.search.windows.net/indexes/hireme3/docs?api-version=2020-06-30&search=',
+            sendEventPostUri: '',
+            terminatePostUri: '',
+            purgeHistoryDeleteUri: '',
+            restartPostUri: '',
+            candidates: []
+          },
+          showTriggerApiResponseTable: props.showTriggerApiResponseTable,
+          triggerApiResponseStatus: ''
+        };
+    
+        this.handleQueryChange = this.handleQueryChange.bind(this);
+        this.handleRecruiterEmailIdChange = this.handleRecruiterEmailIdChange.bind(this);
+        this.handleJdLinkChange = this.handleJdLinkChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      };
+
+      componentDidUpdate(prevProps, prevState) {
+        if (prevState.showTriggerApiResponseTable !== this.props.showTriggerApiResponseTable) {
+          this.setState({
+            showTriggerApiResponseTable: this.props.showTriggerApiResponseTable
+          });
+        }
+      }
+
+
+      handleQueryChange(event) {
+        var triggerApiRequestParams = this.state.triggerApiRequestParams;
+        triggerApiRequestParams.query = event.target.value;
+        this.setState({triggerApiRequestParams});
+      }
+
+      handleRecruiterEmailIdChange(event) {
+        var triggerApiRequestParams = this.state.triggerApiRequestParams;
+        triggerApiRequestParams.recruiterEmailId = event.target.value;
+        this.setState({triggerApiRequestParams});
+      }
+
+      handleJdLinkChange(event) {
+        var triggerApiRequestParams = this.state.triggerApiRequestParams;
+        triggerApiRequestParams.jdlink = event.target.value;
+        this.setState({triggerApiRequestParams});
+      }
+    
+      async handleSubmit(event) {
+        event.preventDefault();
+
+        const queryURI = this.state.triggerApiRequestParams.queryURI + this.state.triggerApiRequestParams.query;
+        const requestOptions = {
+          method: 'GET',
+          mode: 'cors', 
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'api-key': '8Lu2Go84TxcuxoPZx7DuWI6og06YPC1D3laNvbQwjIAzSeBtnM8r'
+          }
+        };
+
+        const response = await fetch(queryURI, requestOptions);
+        const data = await response.json();
+        console.log('fetched data: response: ' + JSON.stringify(data));
+
+        if (data !== null) {
+          let triggerApiResponseStatus = 'fetched data: response status: ';
+          this.setState({
+            showTriggerApiResponseTable: true,
+            triggerApiResponseStatus: triggerApiResponseStatus
+          });
+          this.props.parentCallback({showTriggerApiResponseTable: true});
+
+          const triggerApiResponseParams = {
+
+            data: data.value
+          }
+          console.log(triggerApiResponseParams);
+
+          this.setState({triggerApiResponseParams: triggerApiResponseParams});
+          console.log('inner');
+        }
+        console.log('fetched data: response: ' + JSON.stringify(this.state.triggerApiResponseParams));
+      }
+
+      render() {
+
+        const checkStatus = this.state.showTriggerApiResponseTable;
+        let renderComponent;
+        if (checkStatus) {
+          renderComponent = <CulpritPrTriggerResponse 
+                              triggerApiResponseParams={this.state.triggerApiResponseParams}
+                              parentCallback={this.props.parentCallback}>
+                            </CulpritPrTriggerResponse>
+        } else {
+          renderComponent = <form>
+                              <div className="user-box">
+                                <input type="text" value={this.state.triggerApiRequestParams.recruiterEmailId} onChange={this.handleRecruiterEmailIdChange} />
+                                <label>Email ID</label>
+                              </div>
+                              <div className="user-box">
+                                <input type="text" value={this.state.triggerApiRequestParams.jdlink} onChange={this.handleJdLinkChange} />
+                                <label>Job Description URL</label>
+                              </div>
+                              <div className="user-box">
+                                <input type="text" value={this.state.triggerApiRequestParams.query} onChange={this.handleQueryChange} />
+                                <label>Query</label>
+                              </div>
+                              <a onClick={this.handleSubmit}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Submit
+                              </a>
+
+                              <label>
+                                <span></span>
+                                <span></span>
+                                {this.state.triggerApiResponseStatus}
+                              </label>
+                            </form>;
+        }
+
+        return (
+          <div>
+            {renderComponent}
+          </div>
+        );
+      }
+}
+
+export default TriggerCulpritPR;
